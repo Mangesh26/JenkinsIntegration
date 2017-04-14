32a1fe967937457b0a96f127118aed6cd461d414
502710494 <502710494@G9SJ7JC2E.logon.ds.ge.com>
2017-04-14 12:43:36 +0530

    Add one file

---
+++ Practice/src/tesT/ArrangeAplhabatically.java
@@ -0,0 +1,36 @@
+package tesT;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrangeAplhabatically {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		List<String> listOfCountryNames=new ArrayList<String>(); 
+		
+		
+		
+		listOfCountryNames.add("India");
+		listOfCountryNames.add("China");
+		listOfCountryNames.add("Srilanka");
+		listOfCountryNames.add("NewZealand");
+		
+		Collections.sort(listOfCountryNames);
+		
+		Iterator<String> itr=listOfCountryNames.iterator();
+		
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+			            
+		}
+		
+		
+	}
+
+}
